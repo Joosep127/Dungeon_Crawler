166892668689ceaabc05a5374075f0b9021da6e0
Joosep127 <joosep.tomingas@gmail.com>
2023-11-01 20:24:59 +0200

    Completed player class selection for now. Added spells and inventory functionality on the player object. Started on the game beginning in main.py

--- Classes/player.py
+++ Classes/player.py
@@ -1,19 +1,17 @@
 from math import floor
-
+import os
+import time
+clear = lambda: os.system('cls')
 class Player:
-    
-    multipliers = {
-    "xp": 1.1,
-    "dmg": 1,
-    "hp": 2, #default 2
-    "lvl_up": 1.1, #default 1.5
-    "coin": 1.0
-}
     equipment = {
-    
+        "Helmet": None,
+        "Chestplate": None,
+        "Leggings": None,
+        "Boots": None,
+        "Sword": None    
 }
     
-    def __init__(self, name, health, mana, damage, coin):
+    def __init__(self, name, health, mana, damage, coin, clss):
         self.name = name
         self.health = health
         self.max_health = health
@@ -23,9 +21,20 @@ class Player:
         self.xp = 0
         self.level = 1
         self.coin = coin
+        self.clss = clss
+        self.multipliers = {
+            "xp": 1.0,
+            "dmg": 2,
+            "hp": 2, #default 2
+            "mana": 2, #default 2
+            "lvl_up": 1.5, #default 1.5
+            "coin": 1.0
+        }
+        self.spells = {}
+        self.inventory = {}
     
     def add_coin(self, add):
-        self.coin += add*self.multipliers["coin"]
+        self.coin += add * self.multipliers["coin"]
         
     def lose_hp(self, lost):
         self.health -= lost
@@ -39,13 +48,134 @@ class Player:
             self.health = self.max_health
         
     def add_xp(self, add):
-        self.xp += floor(add*self.multipliers["xp"])
+        xp_gained = floor(add * self.multipliers["xp"])
+        self.xp += xp_gained
         base_xp = floor(20 * (self.multipliers["lvl_up"] ** (self.level - 1)))
+
         while self.xp >= base_xp:
             self.xp -= base_xp
             self.level += 1
             self.max_health += self.multipliers["hp"]
             self.health = self.max_health
+            self.max_mana += self.multipliers["mana"]
+            self.mana = self.max_mana
             self.damage += self.multipliers["dmg"]
-            print('Level up!')
+            return(["Levelup", xp_gained])
 
+        return([xp_gained])
+    
+    def add_spell(self, spell):
+        if spell in self.spells:
+            self.spells[spell] += 1
+        else:
+            self.spells[spell] = 1
+    
+    def add_inventory(item):
+        if item in self.inventory:
+            self.inventory[item] += 1
+        else:
+            self.inventory[item] = 1
+    
+    def lose_inventory(item):
+        i = self.inventory.get(item, 0)
+        if i > 0:
+            i -= 1
+            self.inventory[item] = i
+            if i == 0:
+                del self.inventory[item]
+
+def Change_name():
+    while True:
+        name = input("Enter your name: ")
+        if len(name) > 20:
+            print("Name is too long. It must be 20 characters or less.")
+        else:
+            try:
+                name.encode('ascii')
+            except UnicodeEncodeError:
+                print("Name contains non-ASCII characters. Please enter a valid Player name.")
+            else:
+                return(name.title())
+
+def Create():
+    name = Change_name()
+    game_classes = {
+    'The Regular Guy': {
+        'name': '*Has a ketchup stain on his shirt',
+        'health': 100,
+        'mana': 50,
+        'damage': 20,
+        'coin': 100,
+        'multipliers': {}
+    },
+    'The Thief': {
+        'name': '*Is decked out with handbags and whatnot from having stolen soo much loot from old ladies',
+        'health': 80,
+        'mana': 100,
+        'damage': 30,
+        'coin': 50,
+        'multipliers': {"coin": 2.0}
+    },
+    'The Knight': {
+        'name': "*Despite the sword's steel blade haven fallen off from countless battles, he still uses the wooden handle to beat monsters into submission",
+        'health': 150,
+        'mana': 30,
+        'damage': 40,
+        'coin': 200,
+        'multipliers': {}
+    },
+    'The Rogue': {
+        'name': '*Fast and agile, adapts.',
+        'health': 60,
+        'mana': 70,
+        'damage': 25,
+        'coin': 75,
+        'multipliers': {"xp": 2.7, "dmg": 2, "hp": 4, "lvl_up": 1.35,}
+    },
+    'The Physically Ill': {
+        'name': '*Has the sniffles ): ',
+        'health': 50,
+        'mana': 10,
+        'damage': 10,
+        'coin': 10,
+        'multipliers': {"xp": 0.7, "dmg": 1, "hp": 1, "mana": 1, "lvl_up": 1.65, "coin": 0.7}
+    }    
+}
+    while True:
+        clear()
+        print(f'Hello {name}. Please choose your character class: ')
+        for x, (j, i) in enumerate(game_classes.items()):
+            print(f'[{x + 1}] : {j} ({i["name"]}) | \n'
+                  f'                    Hp: {i["health"]} \n'
+                  f'                    Mana: {i["mana"]} \n'
+                  f'                    Dmg: {i["damage"]} \n'
+                  f'                    Coin: {i["coin"]}')
+        choice = input("Enter the number corresponding to your choice: ")
+
+        if choice.isdigit() and 1 <= int(choice) <= len(game_classes):
+            selected_class = list(game_classes.keys())[int(choice) - 1]
+            print(f'You have chosen: {selected_class}') 
+            time.sleep(1)
+            break
+        else:
+            print("Invalid choice. Please enter a valid number.") 
+            time.sleep(1)
+
+    while True:
+        clear()
+        print(f"{name}, {selected_class}")
+        answer = input("Is this correct? [yes, no]: ").lower()
+        if answer == "yes":
+            break
+        elif answer == "no":
+            clear()
+            return Create()
+
+    selected_class_data = game_classes[selected_class]
+    multipliers = selected_class_data["multipliers"]
+    player = Player(name, selected_class_data["health"], selected_class_data["mana"], selected_class_data["damage"], selected_class_data["coin"], selected_class)
+
+    for j, i in multipliers.items():
+        player.multipliers[j] = i
+
+    return player
--- main.py
+++ main.py
@@ -1,76 +1,37 @@
 import os
 from math import floor
-from Classes.player import Player
+from Classes.player import Create
+import time
 clear = lambda: os.system('cls')
 
-def Change_name():
-    while True:
-        name = input("Enter your name: ")
-        if len(name) > 20:
-            print("Name is too long. It must be 20 characters or less.")
-        else:
-            try:
-                name.encode('ascii')
-            except UnicodeEncodeError:
-                print("Name contains non-ASCII characters. Please enter a valid Player name.")
-            else:
-                break
-        return(name)
-
-def Create():
-    name = Change_name()  
-    game_classes = {
-    'The Regular Guy': {
-        'name': '*Has a ketchup stain on his shirt',
-        'health': 100,
-        'mana': 50,
-        'damage': 20,
-        'coin': 100
-    },
-    'The Thief': {
-        'name': '*Is decked out with handbags and whatnot from having stolen soo much loot from old ladies',
-        'health': 80,
-        'mana': 100,
-        'damage': 30,
-        'coin': 50
-    },
-    'The Knight': {
-        'name': "*Despite the sword's steel blade haven fallen off from countless battles, he still uses the wooden handle to beat monsters into submission",
-        'health': 150,
-        'mana': 30,
-        'damage': 40,
-        'coin': 200
-    },
-    'The Rogue': {
-        'name': '*Fast and agile ',
-        'health': 90,
-        'mana': 70,
-        'damage': 25,
-        'coin': 75
-    },
-    'The Physically Ill': {
-        'name': 'The Physically Ill',
-        'health': 50,
-        'mana': 10,
-        'damage': 10,
-        'coin': 10
-    }
-}
-    while True:
-        clear()
-        print('''Hello {name}. Please choose your characters class: ''')
-        for j,i in game_classes.items():
-            print()
-        a = input("")
-
-
-Create()
-#user = Player('Peeter', 100, 20, 1, 0)
-
-
-while True:
+def Hud():
     clear()
-    print(f'name: {user.name} hp: {user.health}/{user.max_health} || xp: {floor(user.xp)}p({floor(20 * (user.multipliers["lvl_up"]**(user.level-1)) - user.xp)}p left) level: {user.level}')
+    print(f"{player.name}, {player.clss}")
+    print(f"Hp: {player.health}/{player.max_health} | Magic: {player.mana}/{player.max_mana} | {player.coin}$ | Lvl: {player.level}")
+    #xp_needed = floor(20 * (player.multipliers["lvl_up"] ** (player.level - 1)) - player.xp)
+    #print(f"xp: {player.xp}\n")
+
+def Main():
+    global player
+    player = Create()
+
+    Hud()
+    if player.clss == 'The Physically Ill':
+        print("Being not only Physically handicapped it appears you are also mentally retarded, deciding to go on an adventure to the local hole in the ground.\n")
+    else:
+        print("You one day decide to go to work, but you fell in to a hole.\n")
+    input("[Press enter to continue]")
+    Hud()
+
+    
+
+    while True: # Game Loop
+        break
+
+
+while False:
+    clear()
+    print(f'name: {user.name} hp: {user.health}/{user.max_health} || xp: {floor(20 * (user.multipliers["lvl_up"] ** (user.level - 1)) - user.xp)}p left) level: {user.level}')
     a = int(input('''Select what you want to do:
                     1: lose hp
                     2: gain hp
@@ -80,6 +41,10 @@ while True:
     elif a == 2:
         user.add_hp(int(input('amount to add: ')))
     elif a == 3:
-        user.add_xp(int(input('amount to add: ')))
+        print(user.add_xp(int(input('amount to add: ')))[-1])
+        time.sleep(1)
     else:
         break
+
+if __name__ == "__main__":
+    Main()
